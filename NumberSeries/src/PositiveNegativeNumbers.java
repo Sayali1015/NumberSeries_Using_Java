a8b26ae68b18e5b7f783bd2fbe527be46fdbf8fc
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-20 20:50:31 +0530

    Positive Negative Number Code

---
+++ NumberSeries/src/PositiveNegativeNumbers.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class PositiveNegativeNumbers {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+			System.out.println("Enter the number: ");
+				int num = scan.nextInt();
+				//System.out.println(num);
+					if (num >=0) {
+						System.out.println(num + " is the number is Positive");
+					}
+					else {
+						System.out.println(num + " is the number is Negative");
+	
+					}
+
+				}
+		}
+	
+	
