e9e98d635154b4f001d37e683c3b3878499a5de0
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-25 16:14:40 +0530

    Asci Value Of Alphabets

---
+++ NumberSeries/src/AsciValue.java
@@ -0,0 +1,45 @@
+//class AsciValue 
+//{
+//    public static void main(String[] args) 
+//    {
+//        for(char ch='A' ; ch <= 'Z' ; ch++){
+//            System.out.println(ch + " Ascii value is " +(int)ch);
+//        }
+//    }
+//}
+
+
+
+import java.util.Scanner;
+class AsciValue{
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Alphabet: ");
+		
+		for(char ch ='A' ; ch <='Z'; ch++) {
+		
+			System.out.println(ch + " Ascii value is " +(int)ch);		
+		
+	}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
