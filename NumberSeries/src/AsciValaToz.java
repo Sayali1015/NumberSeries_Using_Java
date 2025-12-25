9d47bd05456304ef007de6a62b7fc5f14baebc72
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-25 16:22:25 +0530

    Asci Value of Smaller Alphabets

---
+++ NumberSeries/src/AsciValaToz.java
@@ -0,0 +1,11 @@
+
+public class AsciValaToz {
+
+	public static void main(String[] args) {
+		for(char ch ='a' ; ch <='z'; ch++) {
+			System.out.println(ch+ " Asci value is: "+ (int)ch);
+		}
+
+	}
+
+}
